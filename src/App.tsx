62ac211351034dbeff6e83d05b3254de70d7f777
serkanozak <av.serkanozak@gmail.com>
2024-02-21 00:41:16 +0300

    App and Index updated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { OverlayLoader } from "./components/OverlayLoader/OverlayLoader";
+import RouteDefinitions from "./components/Routes/RouteDefinitions";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<OverlayLoader />
+			<RouteDefinitions />
+		</>
+	);
 }
-
 export default App;

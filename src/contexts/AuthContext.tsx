1465ec32603a7ec9051d8a8f196d1fd200931e2d
mrtcrpr <mrt.crpr@gmail.com>
2024-02-20 22:41:57 +0300

    Context added.

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,13 @@
+import {createContext, useState} from "react";
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = (props: any) => {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	return (
+		<AuthContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
+			{props.children}
+		</AuthContext.Provider>
+	);
+};
